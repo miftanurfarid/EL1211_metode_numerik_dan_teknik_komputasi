4a1be3f188aa26036d5d41c54aaca4d5a7c1b170
mifta nur farid <miftanurfarid@gmail.com>
2018-11-23 16:14:15 +0800

    update

--- code/solxF1.m
+++ code/solxF1.m
@@ -1,32 +1,35 @@
-clearvars; close all; clc;
+% clearvars; close all; clc;
 
+function fxs = solxF1(xs,x,Fx)
 % Solusi Soal No.2
 % Tugas Final
 % Newton Gregory Forward
 
-xs = 1.1875;
+% xs = 1.1875;
 
-x = [
-    1
-    1.5
-    2
-    2.5
-    3
-    3.5
-    4
-    ];
+% x = [
+%     1
+%     1.5
+%     2
+%     2.5
+%     3
+%     3.5
+%     4
+%     ];
 
 fx = zeros(length(x));
 
-fx(:,1) = [
-    -0.369
-    -0.0704
-    0.3407
-    0.1578
-    -0.7106
-    -1.5934
-    -1.643
-    ];
+% fx(:,1) = [
+%     -0.369
+%     -0.0704
+%     0.3407
+%     0.1578
+%     -0.7106
+%     -1.5934
+%     -1.643
+%     ];
+
+fx(:,1) = Fx;
 
 for col = 1:length(x)-1
     for row = 1:length(x)-col
@@ -36,6 +39,8 @@ end
 
 s = calS(xs, x(1), x(2));
 fxs = calNGF(s, fx, length(x));
+
+end
 
 % persamaan NGF
 function y = calNGF(s, fx, pol)
