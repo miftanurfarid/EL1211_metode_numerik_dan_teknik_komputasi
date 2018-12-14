f3418f28e6aad094700a5cd08a0dac409a965478
mifta nur farid <miftanurfarid@gmail.com>
2018-12-14 10:20:38 +0700

    fix

--- code/solxF1.m
+++ code/solxF1.m
@@ -5,29 +5,7 @@ function fxs = solxF1(xs,x,Fx)
 % Tugas Final
 % Newton Gregory Forward
 
-% xs = 1.1875;
-
-% x = [
-%     1
-%     1.5
-%     2
-%     2.5
-%     3
-%     3.5
-%     4
-%     ];
-
 fx = zeros(length(x));
-
-% fx(:,1) = [
-%     -0.369
-%     -0.0704
-%     0.3407
-%     0.1578
-%     -0.7106
-%     -1.5934
-%     -1.643
-%     ];
 
 fx(:,1) = Fx;
 
@@ -37,32 +15,38 @@ for col = 1:length(x)-1
     end
 end
 
-s = calS(xs, x(1), x(2));
-fxs = calNGF(s, fx, length(x));
+fx
+
+s = calS1(xs, x(1), x(2))
+fxs = calNGF1(s, fx, length(x));
 
 end
 
 % persamaan NGF
-function y = calNGF(s, fx, pol)
+function y = calNGF1(s, fx, pol)
     f = fx(1,1);    % f0
     for idx = 2:pol
         if idx == 2
-            f = f + ( s * fx(1,idx) / calFac(idx-1) );
+            % fprintf('delF = %.4f\n',fx(1,idx))
+            f = f + ( s * fx(1,idx) / calFac1(idx-1) );
+            sn = s;
         else
-            s = s * (s - (idx - 2));
-            f = f + ( s * fx(1,idx) / calFac(idx-1) );
+            % fprintf('delF = %.4f\n',fx(1,idx))
+            sn = sn * (s - (idx - 2));
+            f = f + ( sn * fx(1,idx) / calFac1(idx-1) );
         end
+        fprintf('fxs = %.04f\n\n', f);
     end
     y = f;
 end
 
 % hitung S
-function s = calS(xs, x0, x1)
+function s = calS1(xs, x0, x1)
     s = (xs - x0)/(x1 - x0);
 end
 
 % hitung factorial
-function z = calFac(x)
+function z = calFac1(x)
     y = x;
     while y > 1
         x = x * (y - 1);
